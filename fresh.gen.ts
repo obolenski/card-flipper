78e1dd8a997bb36cf0376a4075929b258e09de16
obolenski <kornet.obolenski@gmail.com>
2022-08-29 15:12:49 +0400

    commit manifest

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,17 +2,26 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $0 from "./routes/api/getallcards.ts";
-import * as $1 from "./routes/api/joke.ts";
-import * as $2 from "./routes/index.tsx";
+import * as $0 from "./routes/[name].tsx";
+import * as $1 from "./routes/all.tsx";
+import * as $2 from "./routes/api/joke.ts";
+import * as $3 from "./routes/api/postallcards.ts";
+import * as $4 from "./routes/index.tsx";
+import * as $5 from "./routes/random.tsx";
+import * as $$0 from "./islands/RandomCard.tsx";
 
 const manifest = {
   routes: {
-    "./routes/api/getallcards.ts": $0,
-    "./routes/api/joke.ts": $1,
-    "./routes/index.tsx": $2,
+    "./routes/[name].tsx": $0,
+    "./routes/all.tsx": $1,
+    "./routes/api/joke.ts": $2,
+    "./routes/api/postallcards.ts": $3,
+    "./routes/index.tsx": $4,
+    "./routes/random.tsx": $5,
   },
-  islands: {},
+  islands: {
+    "./islands/RandomCard.tsx": $$0,
+  },
   baseUrl: import.meta.url,
 };
 
