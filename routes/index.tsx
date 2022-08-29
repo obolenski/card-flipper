9b2e39a51d7e88030f837327c32d3b833d44ba18
obolenski <kornet.obolenski@gmail.com>
2022-08-29 15:11:22 +0400

    add simple home page

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,42 +1,24 @@
 /** @jsx h */
 import { h } from "preact";
-import { Handlers, PageProps } from "$fresh/server.ts";
 import { tw } from "@twind";
-import Card from "../components/Card.tsx";
-import { LanguageCard } from "../utils/types.ts";
-import { BaseURL } from "../utils/env.ts";
+import Main from "../components/Main.tsx";
 
-export const handler: Handlers<{
-  randomCard: LanguageCard;
-}> = {
-  async GET(req, ctx) {
-    const cardResponse = await fetchRandomCard();
-    return ctx.render({
-      randomCard: cardResponse,
-    });
-  },
-};
-
-const fetchRandomCard = async (): Promise<LanguageCard> => {
-  const cards = await fetch(`${BaseURL}/api/getallcards`)
-    .then(
-      (res) => res.json(),
-    );
-  const randomIndex = Math.floor(Math.random() * cards.length);
-  return cards[randomIndex];
-};
-
-export default function Home(
-  props: PageProps<{
-    randomCard: LanguageCard;
-  }>,
-) {
-  const { randomCard } = props.data;
+export default function Home() {
   return (
-    <div
-      class={tw`mx-auto max-w-screen-2xl h-screen flex items-center justify-around bg-gradient-to-r from-purple-900 via-indigo-900 to-pink-900`}
-    >
-      <Card card={randomCard} />
-    </div>
+    <Main>
+      <div
+        class={tw`w-full flex flex-col items-center justify-center flex-grow-1`}
+      >
+        <div
+          class={tw`h-[50%] w-[50%] 
+              flex flex-col items-center justify-around
+              text-4xl text-gray-50 text-opacity-40 font-serif font-bold`}
+        >
+          <h1>Hello</h1>
+          <p>Please learn words.</p>
+          <small>Thank you!</small>
+        </div>
+      </div>
+    </Main>
   );
 }
