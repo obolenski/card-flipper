b986034c8d22a4ee7151c319db9e51806e6ac4dc
obolenski <kornet.obolenski@gmail.com>
2022-09-29 18:56:26 +0400

    commit manifest

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -7,16 +7,20 @@ import * as $0 from "./routes/_404.tsx";
 import * as $1 from "./routes/_middleware.ts";
 import * as $2 from "./routes/api/fav.ts";
 import * as $3 from "./routes/api/joke.ts";
-import * as $4 from "./routes/api/postallcards.ts";
+import * as $4 from "./routes/api/submitcard.ts";
 import * as $5 from "./routes/index.tsx";
 import * as $6 from "./routes/logout.ts";
-import * as $7 from "./routes/random.tsx";
-import * as $8 from "./routes/random/[setOfCards].tsx";
-import * as $9 from "./routes/tables.tsx";
-import * as $10 from "./routes/tables/[setOfCards].tsx";
-import * as $11 from "./routes/user.tsx";
-import * as $$0 from "./islands/LikeButton.tsx";
-import * as $$1 from "./islands/RandomCard.tsx";
+import * as $7 from "./routes/submit.tsx";
+import * as $8 from "./routes/tables/[setOfCards].tsx";
+import * as $9 from "./routes/user.tsx";
+import * as $$0 from "./islands/Card.tsx";
+import * as $$1 from "./islands/CardCreator.tsx";
+import * as $$2 from "./islands/CardFlipper.tsx";
+import * as $$3 from "./islands/CollapsibleNavGroup.tsx";
+import * as $$4 from "./islands/LikeButton.tsx";
+import * as $$5 from "./islands/MultiSelect.tsx";
+import * as $$6 from "./islands/MultiSelectOption.tsx";
+import * as $$7 from "./islands/Toggle.tsx";
 
 const manifest = {
   routes: {
@@ -24,18 +28,22 @@ const manifest = {
     "./routes/_middleware.ts": $1,
     "./routes/api/fav.ts": $2,
     "./routes/api/joke.ts": $3,
-    "./routes/api/postallcards.ts": $4,
+    "./routes/api/submitcard.ts": $4,
     "./routes/index.tsx": $5,
     "./routes/logout.ts": $6,
-    "./routes/random.tsx": $7,
-    "./routes/random/[setOfCards].tsx": $8,
-    "./routes/tables.tsx": $9,
-    "./routes/tables/[setOfCards].tsx": $10,
-    "./routes/user.tsx": $11,
+    "./routes/submit.tsx": $7,
+    "./routes/tables/[setOfCards].tsx": $8,
+    "./routes/user.tsx": $9,
   },
   islands: {
-    "./islands/LikeButton.tsx": $$0,
-    "./islands/RandomCard.tsx": $$1,
+    "./islands/Card.tsx": $$0,
+    "./islands/CardCreator.tsx": $$1,
+    "./islands/CardFlipper.tsx": $$2,
+    "./islands/CollapsibleNavGroup.tsx": $$3,
+    "./islands/LikeButton.tsx": $$4,
+    "./islands/MultiSelect.tsx": $$5,
+    "./islands/MultiSelectOption.tsx": $$6,
+    "./islands/Toggle.tsx": $$7,
   },
   baseUrl: import.meta.url,
   config,
