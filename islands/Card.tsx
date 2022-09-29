e8aa155bbef3b6a608b932d728aa62713cbe081e
obolenski <kornet.obolenski@gmail.com>
2022-09-29 18:49:25 +0400

    make Card.tsx an island

--- islands/Card.tsx
+++ islands/Card.tsx
@@ -1,13 +1,13 @@
-import { LanguageCard } from "../utils/types.ts";
+import { LanguageCardVM } from "../utils/types.ts";
 
 interface CardProps {
-  card: LanguageCard;
+  card: LanguageCardVM;
   flipVisibility: boolean;
 }
 
 export default function Card(props: CardProps) {
   return (
-    <div class="cursor-pointer h-3/4 min-h-[40vh] w-full min-w-max relative m-auto ease-in group z-0">
+    <div class="cursor-pointer h-full w-full relative ease-in group z-0">
       <div class="h-full w-full min-w-max absolute 
         bg-white bg-opacity-10 text-gray-200 font-serif font-bold
         rounded-xl shadow-xl
@@ -17,25 +17,23 @@ export default function Card(props: CardProps) {
             ? "h-full w-full min-w-max absolute overflow-hidden flex items-center justify-around flex-col opacity-0 group-hover:opacity-100 transition-all"
             : "h-full w-full absolute overflow-hidden flex items-center justify-around flex-col group-hover:opacity-0 transition-all"}
         >
-          <div class="text-6xl">{props.card.sourceLangText}</div>
+          <div class="text-2xl sm:text-6xl">
+            {props.card.sourceLangText}
+          </div>
         </div>
         <div
           class={props.flipVisibility
             ? "h-full w-full min-w-max absolute overflow-hidden flex items-center justify-around flex-col group-hover:opacity-0 transition-all"
             : "h-full w-full absolute overflow-hidden flex items-center justify-around flex-col opacity-0 group-hover:opacity-100 transition-all"}
         >
-          <div class="text-6xl">{props.card.targetLangText}</div>
-          <div class="text-4xl">
+          <div class="text-2xl sm:text-6xl">{props.card.targetLangText}</div>
+          <div class="text-xl sm:text-4xl">
             {props.card.targetLangTranscription}
           </div>
         </div>
         <div class="absolute bottom-3 block w-full 
             text-center opacity-50 text-xs font-light">
-          hover or press
-          <kbd class="kbd">
-            shift
-          </kbd>
-          to flip the card
+          hover to flip the card
         </div>
       </div>
     </div>
