c176ff36903e89659d0bc86ee86095b397846cac
obolenski <kornet.obolenski@gmail.com>
2022-10-08 21:58:09 +0400

    Revert "cache responses from mongo"
    This reverts commit 7ea7b3b578f7cc63eacc1e1d0795f7b5514eb20a.

--- services/mongoApi.ts
+++ services/mongoApi.ts
@@ -3,16 +3,6 @@ import { MongoAPIBaseUrl, MongoApiKey, MongoCluster } from '../utils/env.ts'
 import { LanguageCard, UserFavs } from '../utils/types.ts'
 
 export const getAllCards = async (): Promise<LanguageCard[]> => {
-  refreshCardsInCache()
-  const cachedCards = await getFromCache('db', 'cards')
-  if (cachedCards) {
-    return cachedCards
-  }
-  const cards = await getAllCardsFromDb()
-  return cards
-}
-
-const getAllCardsFromDb = async (): Promise<LanguageCard[]> => {
   const data = JSON.stringify({
     collection: 'cards',
     database: 'langCards',
@@ -32,9 +22,7 @@ const getAllCardsFromDb = async (): Promise<LanguageCard[]> => {
 
   const cards = await axiod
     .default(config)
-    .then((res) => {
-      return res.data.documents
-    })
+    .then((res) => res.data.documents)
     .catch(function (error) {
       console.log(error)
     })
@@ -42,22 +30,38 @@ const getAllCardsFromDb = async (): Promise<LanguageCard[]> => {
   return cards
 }
 
-const refreshCardsInCache = async () => {
-  const cards = await getAllCardsFromDb()
-  saveToCache('db', 'cards', cards)
+export const getCardsByIds = async (ids: string[]): Promise<LanguageCard[]> => {
+  const data = JSON.stringify({
+    collection: 'cards',
+    database: 'langCards',
+    dataSource: MongoCluster,
+    filter: {
+      _id: { $in: ids },
+    },
+  })
+
+  const config = {
+    method: 'post',
+    url: `${MongoAPIBaseUrl}/find`,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Request-Headers': '*',
+      'api-key': MongoApiKey,
+    },
+    data: data,
+  }
+
+  const cards = await axiod
+    .default(config)
+    .then((res) => res.data.documents)
+    .catch(function (error) {
+      console.log(error)
+    })
+
+  return cards
 }
 
 export const getUserFavs = async (email: string): Promise<UserFavs> => {
-  refreshUserFavsInCache(email)
-  const cachedFavs = await getFromCache('db', `favs-${email}`)
-  if (cachedFavs) {
-    return cachedFavs
-  }
-  const userFavs = await getUserFavsFromDb(email)
-  return userFavs
-}
-
-const getUserFavsFromDb = async (email: string): Promise<UserFavs> => {
   const data = JSON.stringify({
     collection: 'userFavs',
     database: 'langCards',
@@ -88,11 +92,6 @@ const getUserFavsFromDb = async (email: string): Promise<UserFavs> => {
   return userFavs
 }
 
-const refreshUserFavsInCache = async (email: string) => {
-  const favs = await getUserFavsFromDb(email)
-  saveToCache('db', `favs-${email}`, favs)
-}
-
 export const addCardIdToUserFavs = async (email: string, cardId: string) => {
   const data = JSON.stringify({
     collection: 'userFavs',
@@ -119,7 +118,7 @@ export const addCardIdToUserFavs = async (email: string, cardId: string) => {
     },
     data: data,
   }
-  refreshUserFavsInCache(email)
+
   return await axiod.default(config).catch((e) => console.log(e))
 }
 
@@ -152,22 +151,6 @@ export const deleteCardIdFromUserFavs = async (
     },
     data: data,
   }
-  refreshUserFavsInCache(email)
+
   return await axiod.default(config).catch((e) => console.log(e))
-}
-
-const getFromCache = async (cacheName: string, key: string) => {
-  const CACHE = await caches.open(cacheName)
-  const res = await CACHE.match(`https:/localhost/${key}`)
-  return await res?.json()
-}
-
-const saveToCache = async (
-  cacheName: string,
-  key: string,
-  value: Record<string, unknown> | Record<string, unknown>[]
-) => {
-  const CACHE = await caches.open(cacheName)
-  const response = new Response(JSON.stringify(value))
-  await CACHE.put(`https:/localhost/${key}`, response.clone())
 }
